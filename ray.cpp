a593061f98fe2158ce9410fa08331cb00d4ee678
Lokesh Sharma <lokeshhsharma@gmail.com>
2015-10-05 12:57:19 +0000

    Add Film and Color classes

--- ray.cpp
+++ ray.cpp
@@ -4,8 +4,10 @@
 
 using namespace std;
 
-int w = 2;
-int h = 4;
+int w = 200;
+int h = 400;
+int pix = w * h;
+BYTE * pixels = new BYTE[3*pix] ;
 
 class Sample
 {
@@ -29,51 +31,77 @@ public:
       return false;
     }
     // Do it
-    sample.x = pixelNumber / w;
-    sample.y = pixelNumber % w;
+    sample.x = pixelNumber % w;
+    sample.y = pixelNumber / w;
     // Increment
     pixelNumber += 1;
     return true;
   }
 };
 
+class Color
+{
+public:
+  Color(int R = 0, int G = 0, int B = 0)
+  {
+    r = R;
+    g = G;
+    b = B;
+  }
+  
+  int r, g, b;
+  Color operator+(Color& c2)
+  {
+    Color res;
+    res.r = r + c2.r;
+    res.g = g + c2.g;
+    res.b = b + c2.b;
+    return res;
+  }
+  
+  Color operator-(Color& c2)
+  {
+    // will fill later
+  }
+  
+};
+
+class Film
+{
+public:
+  void commit(Sample& sample, Color& color)
+  {
+    int pixelNumber = sample.y * w + sample.x;
+    pixels[pixelNumber * 3]     = color.b;
+    pixels[pixelNumber * 3 + 1] = color.g;
+    pixels[pixelNumber * 3 + 2] = color.r;
+  }
+  
+  void writeImage()
+  {
+    FreeImage_Initialise();
+    string fname = "test.png";
+    FIBITMAP *img = FreeImage_ConvertFromRawBits(pixels, w, h, w * 3, 24, 0xFF0000, 0x00FF00, 0x0000FF, false);
+    FreeImage_Save(FIF_PNG, img, fname.c_str(), 0);
+    FreeImage_DeInitialise();    
+  }
+};
+
 int main(void)
 {
-  // FreeImage_Initialise();
-  //
-  // int w = 2, h = 2;
-  // int pix = w * h;
-  // BYTE * pixels = new BYTE[3*pix] ;
-  // pixels[0] = 255;          // Blue
-  // pixels[1] = 0;
-  // pixels[2] = 0;
-  // pixels[3] = 0;            // Red
-  // pixels[4] = 0;
-  // pixels[5] = 255;
-  // pixels[6] = 0;            // Red
-  // pixels[7] = 0;
-  // pixels[8] = 255;
-  // pixels[9] = 0;            // Black
-  // pixels[10] = 0;
-  // pixels[11] = 0;
-  //
-  // string fname = "test.png";
-  //
-  // FIBITMAP *img = FreeImage_ConvertFromRawBits(pixels, w, h, w * 3, 24, 0xFF0000, 0x00FF00, 0x0000FF, false);
-  // FreeImage_Save(FIF_PNG, img, fname.c_str(), 0);
-  // FreeImage_DeInitialise();
-
   Sampler sampler;
   Sample sample;
-  sampler.getSample(sample);
-  cout << sample.x << " " << sample.y << endl;
-  sampler.getSample(sample);
-  cout << sample.x << " " << sample.y << endl;
-  sampler.getSample(sample);
-  cout << sample.x << " " << sample.y << endl;
-  sampler.getSample(sample);
-  cout << sample.x << " " << sample.y << endl;
-  sampler.getSample(sample);
-  cout << sample.x << " " << sample.y << endl;
+  Film film;
+  
+  Color Blue(0, 0, 255);
+  while (sampler.getSample(sample))
+  {
+    film.commit(sample, Blue);
+  }
+  
+  film.writeImage();
+  
+
+
   cout << "It's working." << endl;
 }
